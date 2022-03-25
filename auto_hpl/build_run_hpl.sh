9e42b034b5ca06db7ae4a7d977e79b4b6224e230
David Valin <dvalin@redhat.com>
2022-03-25 08:35:57 -0400

    Add iterations

--- auto_hpl/build_run_hpl.sh
+++ auto_hpl/build_run_hpl.sh
@@ -603,24 +603,30 @@ rm /tmp/results_auto_hpl_${to_tuned_setting} 2> /dev/null
 mkdir ${RESULTSDIR}
 ln -s ${RESULTSDIR} /tmp/results_auto_hpl_${to_tuned_setting}
 
+run_times=0
+
 if [ $to_pbench -eq 1 ];then
-  source ~/.bashrc
+	source ~/.bashrc
 
-  $TOOLS_BIN/execute_pbench --cmd_executing "$0" $arguments --test auto_hpl --spacing 11
-  cd /tmp
-  cp results_auto_hpl_${to_tuned_setting}.tar results_pbench_auto_hpl_${to_tuned_setting}.tar
+  	$TOOLS_BIN/execute_pbench --cmd_executing "$0" $arguments --test auto_hpl --spacing 11
+  	cd /tmp
+  	cp results_auto_hpl_${to_tuned_setting}.tar results_pbench_auto_hpl_${to_tuned_setting}.tar
 else
-  install_run_hpl
+	for iteration in 0 `seq 1 1 ${iterations}`; do
+  		install_run_hpl
 
-  cd /tmp/results_auto_hpl_${to_tuned_setting}
-  pwd
-  for results in `ls -d *log`; do
-	echo results $results
-	  out_file=`echo $results | sed "s/\.log/\.csv/g"`
-	  cat $results | tr -s ' ' | sed "s/^ //g" | sed "s/ /:/g" > $out_file
-  done
-  cd /tmp
-  tar hcf results_auto_hpl_${to_tuned_setting}.tar results_auto_hpl_${to_tuned_setting}
+  		pushd /tmp/results_auto_hpl_${to_tuned_setting} > /dev/null
+		rdir=results_${iteration}
+		mkdir $rdir
+		mv hpl* $rdir
+		cd $rdir
+  		for results in `ls -d *log`; do
+	  		out_file=`echo $results | sed "s/\.log/\.csv/g"`
+	  		cat $results | tr -s ' ' | sed "s/^ //g" | sed "s/ /:/g" > $out_file
+  		done
+	done
+	cd /tmp
+	tar hcf results_auto_hpl_${to_tuned_setting}.tar results_auto_hpl_${to_tuned_setting}
 fi
 exit 0
 
